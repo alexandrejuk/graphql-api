dcf1f8031c11f1fea3325a269e3e99e456c3414e
alexandrejuk <ale_santos.soares@hotmail.com>
2018-01-30 16:12:00 -0200

    first commit

---
+++ schema.js
@@ -0,0 +1,15 @@
+export default `
+
+    type Cat {
+        _id: String!
+        name: String!
+    }
+
+    type Query {
+        allCats: [Cat!]!
+    }
+
+    type Mutation {
+        createCat(name: String!): Cat!
+    }
+`
